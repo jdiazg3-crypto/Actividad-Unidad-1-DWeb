01b42c0cb9cabd75ce75059017fb5e64d9ce259f
Juanse <juansediazg4@gmail.com>
2026-09-16 17:00:42 -0500

    Permitir usuarios temporalmente para crear usuario de prueba

--- src/main/java/com/unicartagena/EquipoFutbolApp/config/SecurityConfig.java
+++ src/main/java/com/unicartagena/EquipoFutbolApp/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/css/**", "/js/**", "/recuperar-clave", "/enviar-clave").permitAll()
+                        .requestMatchers("/login", "/css/**", "/js/**", "/recuperar-clave", "/enviar-clave", "/usuarios/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
